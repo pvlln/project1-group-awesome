9df3c4a1f62c6f2a4ce005fa0acafae2009c2fe9
Seven <edrezner7@gmail.com>
2023-03-12 16:32:02 -0400

    added poster images and styling in CSS

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var movieInput = document.querySelector("#movieText");
 function displaySearches() {
     var searchArray = JSON.parse(localStorage.getItem("Movie Title")) || [];
     console.log(searchArray)
-    
+
     $("#movieText").autocomplete({
         source: searchArray
     });
 }
 
-movieInput.addEventListener("click", function() {
+movieInput.addEventListener("click", function () {
     displaySearches();
 });
 
@@ -18,11 +18,13 @@ movieInput.addEventListener("click", function() {
 searchBtn.addEventListener("click", function () {
     var movieText = document.querySelector("#movieText").value;
     var movieString = movieText.replace(/\s/g, "+");
-    
+
     var recentSearches = JSON.parse(localStorage.getItem("Movie Title")) || [];
-    if (!recentSearches.includes(movieText)) { 
+
+    if (!recentSearches.includes(movieText)) {
         recentSearches.unshift(movieText);
     }
+
     localStorage.setItem("Movie Title", JSON.stringify(recentSearches));
     console.log(recentSearches);
 
@@ -31,48 +33,42 @@ searchBtn.addEventListener("click", function () {
 
     document.querySelector("#movieText").value = "";
 
-
-    fetch("http://www.omdbapi.com/?apikey=6bb284e5&s=" + movieString + "&type=movie&plot=long&page=1")  
+    var OMDBRequestUrl = "http://www.omdbapi.com/?apikey=695fdf11&s=" + movieString + "&type=movie";
+    
+    fetch(OMDBRequestUrl)
 
         .then(response => response.json())
         .then(data => {
-            
+            console.log(data);
+
             for (var i = 0; i < 2; i++) {
                 var row = document.createElement("div");
                 row.classList.add("row", "my-4");
-        
+
                 container.appendChild(row);
-                
+
                 for (var i = 0; i < 8; i++) {
                     var card = document.createElement("div");
-                    card.classList.add("card", "col-3", "mb-4");
-            
-                    var cardBody = document.createElement("div");
-                    cardBody.classList.add("card-body");
-            
+                    card.classList.add("card", "col-2", "mb-4");
+
                     var cardTitle = document.createElement("h5");
                     cardTitle.classList.add("card-title");
                     cardTitle.textContent = data.Search[i].Title;
-            
+
                     var cardSubtitle = document.createElement("h6");
-                    cardSubtitle.classList.add("card-subtitle");
+                    cardSubtitle.classList.add("card-subtitle", "text-muted");
                     cardSubtitle.textContent = data.Search[i].Year;
-            
-                    var cardText = document.createElement("p");
-                    cardText.classList.add("card-text");
-                    cardText.textContent = data.Search[i].Plot;
-            
-                    cardBody.appendChild(cardTitle);
-                    cardBody.appendChild(cardSubtitle);
-                    cardBody.appendChild(cardText);
-            
-                    card.appendChild(cardBody);
-    
+
+                    var cardPoster = document.createElement("img");
+                    cardPoster.src = data.Search[i].Poster;
+                    cardPoster.classList.add("card-poster");
+
+                    card.appendChild(cardTitle);
+                    card.appendChild(cardSubtitle);
+                    card.appendChild(cardPoster);
+
                     row.appendChild(card);
                 }
             }
         });
-});    
-
-
-
+});
